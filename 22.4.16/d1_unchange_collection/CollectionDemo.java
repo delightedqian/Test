ae1fec96e392c88a1795e3929bc7643ab7a48bab
1753450831@qq.com <1753450831@qq.com>
2022-04-16 20:48:46 +0800

    22.4.16

---
+++ 22.4.16/d1_unchange_collection/CollectionDemo.java
@@ -0,0 +1,17 @@
+package io.delightedqian.d1_unchange_collection;
+
+
+import java.util.List;
+
+/**
+ * Project Name: CollectionDemo
+ * Description:
+ * USER: ChenXiaoqian
+ * DATE:  2022/4/16
+ * TIME:  20:19
+ */
+public class CollectionDemo {
+    public static void main(String[] args) {
+
+    }
+}
