44222ec12002e644dc8e019f031c47de0f5622a6
1753450831@qq.com <1753450831@qq.com>
2022-06-04 19:08:24 +0800

    22.6.4

---
+++ 22.6.4/maven/maven_demo/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Project Name: HelloWorld
+ * Description:
+ * USER: ChenXiaoqian
+ * DATE:  2022/6/4
+ * TIME:  15:46
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+    }
+}
